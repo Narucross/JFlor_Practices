8becdb8198ee167801e68a14cbd4ed3f4402081d
Joshua Flores <jflores@student.neumont.edu>
2014-01-03 05:29:28 -0800

    Tabbed_Panes_3_DataObjects
    Resetted the dataobjects

--- Cap_Fall_prc/TabbedPanes/TabbedPane_Excercise_1/TabbedPane_Excercise_1/Views/ScreenRep/ScreenTabItem.xaml.cs
+++ Cap_Fall_prc/TabbedPanes/TabbedPane_Excercise_1/TabbedPane_Excercise_1/Views/ScreenRep/ScreenTabItem.xaml.cs
@@ -13,26 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TabbedPane_Excercise_1
+namespace TabbedPane_Excercise_1.Views.ScreenRep
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for ScreenTabItem.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class ScreenTabItem : UserControl
     {
-        public MainWindow()
+        public ScreenTabItem()
         {
             InitializeComponent();
-        }
-
-        private void fillTabbedDynamic() {             
-            // Code_Flextion_TPanel.Items
-            // Write down the intention of this code...
-            // 
-            int ram = 3;
-            for (int i = 0; i < ram; i++) { 
-            
-            }
         }
     }
 }
